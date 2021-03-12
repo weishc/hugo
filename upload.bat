149d755a432ada91b5cc383330d1a72794d56c07
weishc <zonker0000@gmail.com>
2021-03-12 15:40:33 +0800

    update-hugo-file

--- upload.bat
+++ upload.bat
@@ -8,3 +8,10 @@ git remote add origin https://github.com/weishc/weishc.github.io
 git add .
 git commit -m "update-hugo"
 git push -f origin master
+cd ..
+git init
+git remote rm origin
+git remote add origin https://github.com/weishc/hugo
+git add .
+git commit -m "update-hugo-file"
+git push -f origin master
