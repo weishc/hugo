52a0a390b3ad32a1151da00ec877e2744c8f66b3
weishc <zonker0000@gmail.com>
2021-03-12 15:31:12 +0800

    first-hugo

---
+++ upload.bat
@@ -0,0 +1,10 @@
+set PATH=%PATH%;D:\Hugo\bin
+cd /d D:\Hugo\Sites\weishc
+hugo
+cd public
+git init
+git remote rm origin
+git remote add origin https://github.com/weishc/weishc.github.io
+git add -A
+git commit -m "update-hugo"
+git push -f origin master
